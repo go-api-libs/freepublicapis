bc72231fa78c80f30d001f9ef7fa20e37f5551e5
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-11-17 19:07:54 +0100

    update probe

--- internal/probe/probe.gen.go
+++ internal/probe/probe.gen.go
@@ -1,15 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"time"
+
+	"gopkg.in/dnaeon/go-vcr.v3/recorder"
 )
 
 func main() {
 	start := time.Now()
 	defer func() { log.Printf("Probe took %s", time.Since(start)) }()
 
+	r, err := record()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Stop() // Ensure recorder is saved once we're done
+
 	if err := probe(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func record() (*recorder.Recorder, error) {
+	r, err := recorder.NewWithOptions(&recorder.Options{
+		CassetteName:       fmt.Sprintf("internal/probe/vcr/%s", time.Now().Format(time.DateOnly)),
+		Mode:               recorder.ModeReplayWithNewEpisodes,
+		SkipRequestLatency: true,
+		RealTransport:      http.DefaultTransport,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: maskAuthorization
+	// r.AddHook(maskAuthorization, recorder.BeforeSaveHook)
+
+	// replace the default transport with the recorder
+	http.DefaultClient.Transport = r
+
+	return r, nil
 }
--- internal/probe/probe.go
+++ internal/probe/probe.go
@@ -1,29 +1,9 @@
 package main
 
-import (
-	"net/http"
-
-	"gopkg.in/dnaeon/go-vcr.v3/recorder"
-)
+import "net/http"
 
 func probe() error {
-	// Start our recorder
-	r, err := recorder.NewWithOptions(&recorder.Options{
-		CassetteName:       "fixtures/vcr",
-		Mode:               recorder.ModeReplayWithNewEpisodes,
-		SkipRequestLatency: true,
-		RealTransport:      http.DefaultTransport,
-	})
-	if err != nil {
-		return err
-	}
-	defer r.Stop() // Ensure recorder is stopped once done with it
-
-	// TODO: maskAuthorization
-	// r.AddHook(maskAuthorization, recorder.BeforeSaveHook)
-
-	// Inject the transport
-	// notion.SetTransport(r)
+	http.Get()
 
 	return nil
 }
