328673ee93b76cedd8f2ae87b03d9e612d048ea7
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-11-17 19:26:25 +0100

    Update probe.go

--- internal/probe/probe.go
+++ internal/probe/probe.go
@@ -5,7 +5,10 @@ import "net/http"
 // TODO: generate from openapi.json
 const defaultServerURL = "https://www.freepublicapis.com/api/"
 
+// probe calls the API server to check what we can do
 func probe() error {
+	// define http calls here, e.g.: http.Get(defaultServerURL + "my-endpoint")
+
 	_, err := http.Get(defaultServerURL + "random")
 	return err
 }
