d27bb68ad3424fe2e33d67fdc528be37958ba886
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-11-23 01:07:31 +0100

    Update client.gen_test.go

--- pkg/freepublicapis/client.gen_test.go
+++ pkg/freepublicapis/client.gen_test.go
@@ -6,9 +6,12 @@ package freepublicapis_test
 import (
 	"context"
 	"errors"
+	"io"
 	"net/http"
+	"strings"
 	"testing"
 
+	"github.com/go-api-libs/api"
 	"github.com/go-api-libs/freepublicapis/pkg/freepublicapis"
 )
 
@@ -38,5 +41,45 @@ func TestClient_Error(t *testing.T) {
 		} else if !errors.Is(err, testErr) {
 			t.Fatalf("want: %v, got: %v", testErr, err)
 		}
+	})
+
+	t.Run("Unmarshal", func(t *testing.T) {
+		errDecode := &api.DecodingError{}
+
+		t.Run("GetRandom", func(t *testing.T) {
+			// unknown status code
+			http.DefaultClient.Transport = &testRoundTripper{rsp: &http.Response{StatusCode: http.StatusTeapot}}
+
+			if _, err := c.GetRandom(ctx); err == nil {
+				t.Fatal("expected error")
+			} else if !errors.Is(err, api.ErrUnknownStatusCode) {
+				t.Fatalf("want: %v, got: %v", api.ErrUnknownStatusCode, err)
+			}
+
+			// unknown content type
+			http.DefaultClient.Transport = &testRoundTripper{rsp: &http.Response{
+				Header:     http.Header{"Content-Type": []string{"foo"}},
+				StatusCode: http.StatusOK,
+			}}
+
+			if _, err := c.GetRandom(ctx); err == nil {
+				t.Fatal("expected error")
+			} else if !errors.Is(err, api.ErrUnknownContentType) {
+				t.Fatalf("want: %v, got: %v", api.ErrUnknownContentType, err)
+			}
+
+			// decoding error for known content type "application/json"
+			http.DefaultClient.Transport = &testRoundTripper{rsp: &http.Response{
+				Body:       io.NopCloser(strings.NewReader("{")),
+				Header:     http.Header{"Content-Type": []string{"application/json"}},
+				StatusCode: http.StatusOK,
+			}}
+
+			if _, err := c.GetRandom(ctx); err == nil {
+				t.Fatal("expected error")
+			} else if !errors.As(err, &errDecode) {
+				t.Fatalf("want: %v, got: %v", errDecode, err)
+			}
+		})
 	})
 }
