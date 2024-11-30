abf8edf453a55bae452f88772f57fd4f7fbd502f
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-11-30 23:50:36 +0100

    update probe

--- internal/probe/probe.go
+++ internal/probe/probe.go
@@ -12,26 +12,19 @@ import (
 
 // probe calls the API server to check what we can do
 func probe() error {
-	// define http calls here, e.g.: http.Get(defaultServerURL + "my-endpoint")
-
 	c, err := freepublicapis.NewClient()
 	if err != nil {
 		return err
 	}
 
 	ctx := context.Background()
-	randomAPI, err := c.GetRandom(ctx)
-	if err != nil {
-		return err
-	}
-	_ = randomAPI
-
-	myAPI, err := c.GetAPI(ctx, 275)
-	if err != nil {
+	if _, err := c.GetRandom(ctx); err != nil {
 		return err
 	}
 
-	fmt.Printf("api: %#v\n", myAPI)
+	if _, err := c.GetAPI(ctx, 275); err != nil {
+		return err
+	}
 
 	if _, err := c.GetAPI(ctx, 1); err == nil {
 		return fmt.Errorf("expected error")
@@ -43,24 +36,12 @@ func probe() error {
 		return err
 	}
 
-	apis, err := c.ListApis(ctx, &freepublicapis.ListApisParams{
-		Limit: 1000,
+	if _, err := c.ListApis(ctx, &freepublicapis.ListApisParams{
+		Limit: 350,
 		Sort:  freepublicapis.SortBest,
-	})
-	if err != nil {
+	}); err != nil {
 		return err
 	}
-
-	for _, api := range apis {
-		if api.Documentation.String() != api.Source.String() {
-			fmt.Printf("%v\nDoc: %s\nSource: %s\n", api.Title, &api.Documentation, &api.Source)
-		}
-	}
-
-	// TODO:
-	// - ReadMe generation (use ReadMes from other projects as example)
-	// - next APIs
-	// - script that checks previous APIs, generates all 350 APIs
 
 	return nil
 }
